2ac52fe3b93dd4edd312691d5f40b4f87758f7c9
beznavorotov <beznavorotov@gmail.com>
2024-08-21 22:21:09 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var partnersCarousel = document.querySelector('#partnersCarousel');
+  var carousel = new bootstrap.Carousel(partnersCarousel, {
+    interval: 2000,
+    ride: 'carousel'
+  });
+});
+
+
+
+
